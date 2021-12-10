1944da51f62611a1529de3782d3a686c51c04388
YUXINLU <yuxin.lu@thoughtworks.com>
2021-12-10 14:41:12 +0800

    医院设置-分页条件查询

--- service/service_hosp/src/main/java/com/lyx/yygh/hosp/controller/HospitalSetController.java
+++ service/service_hosp/src/main/java/com/lyx/yygh/hosp/controller/HospitalSetController.java
@@ -1,11 +1,15 @@
 package com.lyx.yygh.hosp.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.lyx.yygh.common.result.Result;
 import com.lyx.yygh.model.hosp.HospitalSet;
 import com.lyx.yygh.hosp.service.HospitalSetService;
+import com.lyx.yygh.vo.hosp.HospitalSetQueryVo;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -33,6 +37,31 @@ public class HospitalSetController {
     public Result removeHospSet(@PathVariable Long id) {
         boolean flag = hospitalSetService.removeById(id);
         return flag ? Result.ok() : Result.fail();
+    }
+
+    //3.分页条件查询
+    @PostMapping("findHospitalSetByPage/{current}/{limit}")
+    public Result findHospitalSetByPage(@PathVariable long current, @PathVariable long limit,
+                                        @RequestBody(required = false) HospitalSetQueryVo hospitalSetQueryVo) {
+        //3.1 创建page对象，传递当前页，和每页记录数
+        Page<HospitalSet> page = new Page<>(current,limit);
+        //3.2 分页条件
+        QueryWrapper<HospitalSet> wrapper = new QueryWrapper<>();
+        String hosname = hospitalSetQueryVo.getHosname();//医院名称
+        String hoscode = hospitalSetQueryVo.getHoscode();//医院编号
+        if(!StringUtils.isEmpty(hosname)) {
+            wrapper.like("hosname",hospitalSetQueryVo.getHosname());
+        }
+        if(!StringUtils.isEmpty(hoscode)) {
+            wrapper.eq("hoscode",hospitalSetQueryVo.getHoscode());
+        }
+
+        //3.3 调用分页查询方法
+        Page<HospitalSet> pageHospitalSet = hospitalSetService.page(page, wrapper);
+        //3.4 得到返回结果
+        return Result.ok(pageHospitalSet);
+
+
     }
 
 
